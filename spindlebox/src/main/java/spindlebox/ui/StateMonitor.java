116b436620c11a0f479e1ae6b0fde58cac69e9e1
Dan Boitnott <dboitnot@gmail.com>
2014-11-18 09:56:01 -0600

    Added SHUTTING_DOWN state

--- spindlebox/src/main/java/spindlebox/ui/StateMonitor.java
+++ spindlebox/src/main/java/spindlebox/ui/StateMonitor.java
@@ -2,7 +2,7 @@ package spindlebox.ui;
 
 public interface StateMonitor {
     public static enum State {
-        DISCONNECTED, CONNECTING, PROCESSING, WAITING, PROBLEM
+        DISCONNECTED, CONNECTING, PROCESSING, WAITING, PROBLEM, SHUTTING_DOWN
     }
 
     public void stateChanged(State newState);
